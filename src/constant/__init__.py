1384d556aee27c6d9194495c91098e656098334c
Prince Kumar <pkumar052@rku.ac.in>
2026-08-13 19:03:05 +0530

    constants

---
+++ src/constant/__init__.py
@@ -0,0 +1,8 @@
+import os
+APPLICATION_TRAIN_PATH = os.path.join("data", "application_train.csv")
+APPLICATION_TEST_PATH = os.path.join("data", "application_test.csv")
+artifact_folder = "artifacts"
+
+MODEL_FILE_NAME = "model.pkl"
+MODEL_FILE_EXTENSION = ".pkl"
+TARGET_COLUMN = "TARGET"
